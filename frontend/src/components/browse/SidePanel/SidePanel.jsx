c49d322c9afd31336e84200915229dc9db59f414
qwikk <44299295+qwakk@users.noreply.github.com>
2023-11-22 14:48:59 +0100

    Changed backgroundcolor for the sidepanel in browse

--- frontend/src/components/browse/SidePanel/SidePanel.jsx
+++ frontend/src/components/browse/SidePanel/SidePanel.jsx
@@ -13,6 +13,9 @@ const drawerWidth = 300;
 export default function SidePanel({ handleFilterChange, genre, updateGenre, actors, updateActors}) {
   return (
       <Drawer
+        PaperProps={{sx :{
+          backgroundColor: "rgb(24, 26, 27)"
+        }}}
         sx={{
           width: drawerWidth,
           flexShrink: 0,
@@ -25,19 +28,19 @@ export default function SidePanel({ handleFilterChange, genre, updateGenre, acto
         anchor="left"
       >
         <List style={{marginTop: "55px"}}>
-          <ListItem key={"TODO"} disablePadding>
+          <ListItem key={"TODO"}>
               <ListItemText primary={"Search for movies"} sx={{textAlign: "center"}} />
           </ListItem>
         </List>
         <List>
-          <ListItem key="title" disablePadding style={{paddingBottom: 10}}>
+          <ListItem key="title">
             <TextField sx={{width: 500}} label="Title"></TextField>
           </ListItem>
-          <ListItem key="genre" disablePadding style={{paddingBottom: 10}}>
+          <ListItem key="genre">
             <GenreSelect handleFilterChange={(key,value) => handleFilterChange(key, value)} 
             genre={genre} updateGenre={value => updateGenre(value)}/>
           </ListItem>
-          <ListItem key="actor" disablePadding>
+          <ListItem key="actor">
             <ActorSelect handleFilterChange={(key,value) => handleFilterChange(key, value)} 
             actors={actors} updateActors={value => updateActors(value)}/>
           </ListItem>
